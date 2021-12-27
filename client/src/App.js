01633741ca404eaa44cf8624db560ad3ea47fa0f
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-27 13:38:46 +0100

    feat: add and setup styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
+import { GlobalStyle } from "./GlobalStyles";
 
 function App() {
   return (
-    <div>
-    </div>
+    <App>
+    <GlobalStyle />
+    </App>
   );
 }
 
---
+++ client/src/components/View.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const View = styled.section`
+ flex: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  scroll-snap-align: start;
+  position: relative;
+`
