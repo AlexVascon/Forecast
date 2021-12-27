441ccf86841ebef89711cf7f6bccb1dba1f0fa5d
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-27 15:54:13 +0100

    feat: initialised server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import path from 'path'
+import express from 'express'
+const server = express()
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+server.set('trust proxy', 1)
+
+server.use(cors({origin: process.env.CLIENT_URL}))
+server.use(express.json())
+server.use(express.urlencoded({ extended: false }))
+server.use(cookieParser())
+
+const PORT = process.env.PORT || 5005
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`)
+})
