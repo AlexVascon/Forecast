872ce65efac615cf63d5ba9aa43ecddc9a698adb
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-27 23:24:13 +0100

    feat: add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react'
 import { AppContainer } from './components/AppContainer'
+import Nav from './components/Nav'
 import { GlobalStyle } from './GlobalStyles'
 import Live from './views/Live'
 import Search from './views/Search'
@@ -8,8 +9,9 @@ import { WeatherContext } from './weatherContext'
 function App() {
   const {live} = useContext(WeatherContext)
   return (
-    <AppContainer>
+    <AppContainer id='app'>
     <GlobalStyle />
+    <Nav />
     <Search />
     {live && <Live />}
     </AppContainer>
