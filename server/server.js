61650e91314da193151d7fffc6c24eea1cbbd137
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2022-01-02 21:57:16 +0100

    bye bye cors

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ import weatherRoutes from './routes.js'
 
 server.set('trust proxy', 1)
 
-server.use(cors({ origin: process.env.CLIENT_URL }))
 server.use(express.json())
 server.use(express.urlencoded({ extended: false }))
 server.use(cookieParser())
